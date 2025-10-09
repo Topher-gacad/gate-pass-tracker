7a07362a1b969c44bfd1d8a41f7bdf7644bbdb10
mckl18 <antoniomickel732@gmail.com>
2025-10-09 18:08:51 +0800

    fix: adding guest middleware and Create user resource route

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,11 +7,11 @@ use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    public function showLoginForm()
+    public function show()
     {
         return view('login');
     }
-
+         
     public function login(Request $request)
     {
         $credentials = $request->validate([
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
-
 <?php
 
 use App\Http\Controllers\TimeTrackController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\UserController;
 
+Route::get('login', [LoginController::class, 'showLoginForm'])
+    ->middleware('guest')
+    ->name('login');
 
-Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login'])
+    ->middleware('guest')
+    ->name('login.post');
 
-Route::post('login', [LoginController::class, 'login'])->name('login.post');
-
-Route::post('logout', [LogoutController::class, 'logout'])->name('logout')->middleware('auth');
+Route::post('logout', [LogoutController::class, 'logout'])
+    ->name('logout')
+    ->middleware('auth');
 
 
 Route::get('dashboard', function () {
@@ -23,4 +28,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::resource('time-tracks', TimeTrackController::class);
+Route::resource('users', UserController::class);
+
+
  
