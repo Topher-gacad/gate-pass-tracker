6259c3df18e229f4c61fb004bde84c0d09ddc1f4
mckl18 <antoniomickel732@gmail.com>
2025-10-09 16:30:28 +0800

    feat:Authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
+
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+
+
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+
+Route::post('login', [LoginController::class, 'login'])->name('login.post');
+
+Route::post('logout', [LogoutController::class, 'logout'])->name('logout')->middleware('auth');
+
+
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
