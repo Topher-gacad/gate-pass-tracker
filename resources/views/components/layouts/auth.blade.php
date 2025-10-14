64c7c0925dd34237f89a1e88a167707c026319fb
Shinpachi1009 <jamaricjes@gmail.com>
2025-10-14 15:46:29 +0800

    feat: added filament

--- resources/views/components/layouts/auth.blade.php
+++ resources/views/components/layouts/auth.blade.php
@@ -12,7 +12,7 @@
             <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
                 <div class="size-10 rounded-full">
                     <img
-                        alt="User"
+                        a      lt="User"
                         src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
                 </div>
             </div>
